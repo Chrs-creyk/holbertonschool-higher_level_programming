1f37fd77168c5557bae41a4b4b59b14b194df073
chrs-creyk <chrisacount117@gmail.com>
2023-02-10 12:58:07 -0500

    adding propieties

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,21 +5,20 @@
 class Square:
     """private atribute of an estance"""
 
-    def __init__(self, size=0):
-        if not isinstance(size, int):
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+    
+
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif (value < 0):
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         return self.__size * self.__size
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        self.__value = new_value
