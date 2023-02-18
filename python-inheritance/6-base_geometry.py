e9f637a2973c9fbce26c04c0fd624e0df20b820a
chrs-creyk <chrisacount117@gmail.com>
2023-02-17 21:42:48 -0500

    base geometry 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+area module
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+
+    def area(self):
+        """
+        Raises exception
+        """
+        raise Exception("area() is not implemented")
