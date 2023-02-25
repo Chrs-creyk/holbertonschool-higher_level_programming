efe25621fb0f1fe5d3ab8b335f1c09c3920ef3d4
chrs-creyk <chrisacount117@gmail.com>
2023-02-24 19:26:37 -0500

    adding base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,8 +9,8 @@ class Rectangle(Base):
     '''class Rectangle inherits from base
     '''
     KV_dict = {'id': 'id', 'width': '_Rectangle__width',
-               'height': '_Rectangle__height',
-               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+                'height': '_Rectangle__height',
+                'x': '_Rectangle__x', 'y': '_Rectangle__y'}
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''method __init__ Initialization a Rectangle
@@ -27,8 +27,8 @@ class Rectangle(Base):
         key_list = ['id', '_Rectangle__width', '_Rectangle__height',
                     '_Rectangle__x', '_Rectangle__y']
         KV_dict = {'id': 'id', 'width': '_Rectangle__width',
-                   'height': '_Rectangle__height',
-                   'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+                    'height': '_Rectangle__height',
+                    'x': '_Rectangle__x', 'y': '_Rectangle__y'}
         for idx, el in enumerate(args):
             self.__dict__[key_list[idx]] = el
         if len(args) == 0:
