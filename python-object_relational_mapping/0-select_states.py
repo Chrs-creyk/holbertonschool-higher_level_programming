2b5a2fc6d4aafff0cf849ba2383fb3a35555e089
chrs-creyk <chrisacount117@gmail.com>
2023-03-17 22:19:42 -0500

    selecting states and modd

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Module 0-select_states"""
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+
 import MySQLdb
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
-if __name__ == "__main__":
-    """Main function"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-        )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    for row in cur:
+    connection = MySQLdb.connect(host='localhost', port=3306, user=user,
+                                 passwd=password, db=db_name)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    q_rows = cursor.fetchall()
+    for row in q_rows:
         print(row)
+    cursor.close()
+    connection.close()
