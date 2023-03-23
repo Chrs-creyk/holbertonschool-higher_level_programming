4f1ae850a38ba0e04602efa2ad6f06294213bb54
chrs-creyk <chrisacount117@gmail.com>
2023-03-23 10:32:34 -0500

    upgrading tasks

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module Student
+"""
+
+
+class Student:
+    """
+    defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            new_dict = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+            for i in attrs:
+                if i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            return self.__dict__
