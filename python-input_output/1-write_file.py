8ebcac5301d8238238416bc38cc2fa0bc35115d1
chrs-creyk <chrisacount117@gmail.com>
2023-03-23 10:31:31 -0500

    upgrading tasks

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 Module write_file
 """
 
+
 def write_file(filename="", text=""):
     """
     function that writes a string to a text file (UTF8) and returns the number
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""read lines"""
+"""
+Module append_write
+"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """function"""
-    nlines = len(open(filename).readlines())
-    with open(filename, encoding="utf-8") as file:
-        if nb_lines > 0 and nb_lines < nlines:
-            for lines in range(nb_lines):
-                print(file.readline(), end="")
-        else:
-            print(file.read(), end="")
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text file (UTF8) and
+    returns the number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return (f.write(text))
