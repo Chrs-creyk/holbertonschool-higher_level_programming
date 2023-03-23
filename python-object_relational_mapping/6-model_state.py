2fa22cc862d1a1cfb7cfc20f1f84469bc947cdf7
chrs-creyk <chrisacount117@gmail.com>
2023-03-23 09:35:09 -0500

    test

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+'''
+file that contains the class definition of a State and an instance
+Base = declarative_base()
+'''
 
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    Inherits from Base class
+    Links MySQL table 'state'
+    Instances:
+        id: column of an auto-generated, unique integer
+        name: column of a string with 128 characters maximum
+    '''
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
