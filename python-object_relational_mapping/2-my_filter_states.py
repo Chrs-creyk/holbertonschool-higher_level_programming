8e3b6bc7b9fce495e46d245908682fa4771ff2e5
chrs-creyk <chrisacount117@gmail.com>
2023-04-07 21:18:08 -0500

    adding somethin

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-'''
-script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument
-'''
+"""script that lists all states from the database
+"""
+
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_search = argv[4]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4]
 
-    connection = MySQLdb.connect(host='localhost', port=3306, user=user,
-                                 passwd=password, db=db_name)
-    cursor = connection.cursor()
-    cursor.execute('''
-                   SELECT * FROM states
-                   WHERE Binary name='{}'
-                   ORDER BY id
-                   '''.format(state_search))
-    q_rows = cursor.fetchall()
-    for row in q_rows:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'\
+         ORDER BY id ASC".format(matchName))
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    cursor.close()
-    connection.close()
+    cur.close()
+    conn.close()
