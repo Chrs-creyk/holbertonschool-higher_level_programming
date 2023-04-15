8dfe3a8f86d9e9bc3c7bf7d43f85bc1e1e06492f
Chrs-creyk <chrisacount117@gmail.com>
2023-04-14 20:22:29 -0500

    fixing identation

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const number = parseInt(process.argv[2]);
-
-if (isNaN(number)) {
-  0console.log('Not a number');
+const argNum = process.argv[2];
+if (isNaN(argNum)) {
+  console.log('Not a number');
 } else {
-console.log('My number:', number);
+  console.log('My number: ' + argNum);
 }
