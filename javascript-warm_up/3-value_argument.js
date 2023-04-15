7dbf3f0ddac386b564f8e4591ec74e1f8b7d1f77
Chrs-creyk <chrisacount117@gmail.com>
2023-04-14 19:38:38 -0500

    fixing semistandard 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 const first = process.argv[2];
 
 if (first === undefined) {
-	  console.log('No argument');
+  console.log('No argument');
 } else {
-	  console.log(first);
+  console.log(first);
 }
