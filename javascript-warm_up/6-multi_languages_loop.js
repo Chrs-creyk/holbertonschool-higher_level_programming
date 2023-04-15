eb2cc5e9ccb3a0b00a260d06cf80f1379754afb5
Chrs-creyk <chrisacount117@gmail.com>
2023-04-14 20:23:30 -0500

    fixing identation

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i]);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
+for (idx = 0; idx < 3; idx++) {
+  console.log(arr[idx]);
 }
