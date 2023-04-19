43f934c72a54ec4bf3c19a60b063a640a5105fa2
Chrs-creyk <chrisacount117@gmail.com>
2023-04-19 01:55:30 -0500

    semistandar --fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -6,28 +6,28 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-  }
-  }
-
-    // instance method called print() that prints the rectangle using the character X
-    print () {
-      let x = 0;
-      for (x = 0; x < this.height; x++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-
-    // instance method called rotate() that exchanges the width and the height of the rectangle
-    rotate () {
-      const tmp = this.width;
-      this.width = this.height;
-      this.height = tmp;
-    }
-
-    // instance method called double() that multiples the width and the height of the rectangle by 2
-    double () {
-      this.width *= 2;
-      this.height *= 2;
     }
   }
-  module.exports = Rectangle;
+
+  // instance method called print() that prints the rectangle using the character X
+  print () {
+    let x = 0;
+    for (x = 0; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  // instance method called rotate() that exchanges the width and the height of the rectangle
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  // instance method called double() that multiples the width and the height of the rectangle by 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
