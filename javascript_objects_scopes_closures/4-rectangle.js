955f6180826f9948fafd61555294580d5e2f60d8
Chrs-creyk <chrisacount117@gmail.com>
2023-04-19 01:11:35 -0500

    identation

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // class Rectangle that defines a rectangle
 class Rectangle {
-    // constructor must take 2 arguments w and h
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
+  // constructor must take 2 arguments w and h
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+  }
+  }
+
     // instance method called print() that prints the rectangle using the character X
     print () {
       let x = 0;
@@ -16,14 +16,14 @@ class Rectangle {
         console.log('X'.repeat(this.width));
       }
     }
-  
+
     // instance method called rotate() that exchanges the width and the height of the rectangle
     rotate () {
       const tmp = this.width;
       this.width = this.height;
       this.height = tmp;
     }
-  
+
     // instance method called double() that multiples the width and the height of the rectangle by 2
     double () {
       this.width *= 2;
